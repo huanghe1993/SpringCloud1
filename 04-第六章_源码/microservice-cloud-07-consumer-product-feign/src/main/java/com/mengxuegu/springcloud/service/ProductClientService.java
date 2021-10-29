c463aef1359a743a6fdc180f6bdb8556b5eaeaee
huanghe <huanghe735597346@gmail.com>
2021-10-29 09:11:05 +0800

    Add files via upload

---
+++ 04-第六章_源码/microservice-cloud-07-consumer-product-feign/src/main/java/com/mengxuegu/springcloud/service/ProductClientService.java
@@ -0,0 +1,27 @@
+package com.mengxuegu.springcloud.service;
+
+import com.mengxuegu.springcloud.entities.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * @Auther: 梦学谷
+ */
+@FeignClient(value = "microservice-product") //指定调用的微服务名称
+public interface ProductClientService {
+
+    @RequestMapping(value = "/product/add", method = RequestMethod.POST)
+    boolean add(@RequestBody Product product);
+
+    @RequestMapping(value = "/product/get/{id}", method = RequestMethod.GET)
+    Product get(@PathVariable("id") Long id);
+
+    @RequestMapping(value = "/product/list", method = RequestMethod.GET)
+    List<Product> list();
+
+}
